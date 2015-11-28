d3b31edd5a739a0ebb287c7f4da0513fc823f3b2
Ville Penttinen <villem.penttinen@gmail.com>
2015-11-28 18:26:43 +0200

    Add extra macros

--- Program.fs
+++ Program.fs
@@ -201,6 +201,8 @@
 
         REP env "(def! not (fn* (a) (if a false true)))" |> ignore
         REP env "(def! load-file (fn* (f) (eval (read-string (str \"(do \" (slurp f) \")\")))))" |> ignore
+        REP env "(defmacro! cond (fn* (& xs) (if (> (count xs) 0) (list 'if (first xs) (if (> (count xs) 1) (nth xs 1) (throw \"odd number of forms to cond\")) (cons 'cond (rest (rest xs)))))))" |> ignore
+        REP env "(defmacro! or (fn* (& xs) (if (empty? xs) nil (if (= 1 (count xs)) (first xs) `(let* (or_FIXME ~(first xs)) (if or_FIXME or_FIXME (or ~@(rest xs))))))))" |> ignore
 
         let rec loop () =
             match read "user> " with
