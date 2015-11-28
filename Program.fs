51301064d5cea15d6b7deefb7dd4d4f22c743257
Ville Penttinen <villem.penttinen@gmail.com>
2015-11-29 00:16:54 +0200

    Some minor cleanup

--- Program.fs
+++ Program.fs
@@ -213,24 +213,23 @@
 
         set env "eval" (makePrimitiveFunction(evalFunction env))
 
-//        let malArgs =
-//            let tempArgs = 
-//                if argv.Length = 0 then
-//                    [||]
-//                else
-//                    argv.[1..]
-//            tempArgs
-//            |> Array.map Types.String
-//            |> Array.toList
-//            |> makeList
-
         set env "*ARGV*" (getArgs (List.ofArray argv))
-        set env "*host-language*" (String("F-Sharp"))
+//        set env "*host-language*" (String("F-Sharp"))
 
-        REP env "(def! not (fn* (a) (if a false true)))" |> ignore
-        REP env "(def! load-file (fn* (f) (eval (read-string (str \"(do \" (slurp f) \")\")))))" |> ignore
-        REP env "(defmacro! cond (fn* (& xs) (if (> (count xs) 0) (list 'if (first xs) (if (> (count xs) 1) (nth xs 1) (throw \"odd number of forms to cond\")) (cons 'cond (rest (rest xs)))))))" |> ignore
-        REP env "(defmacro! or (fn* (& xs) (if (empty? xs) nil (if (= 1 (count xs)) (first xs) `(let* (or_FIXME ~(first xs)) (if or_FIXME or_FIXME (or ~@(rest xs))))))))" |> ignore
+//        REP env "(def! not (fn* (a) (if a false true)))" |> ignore
+//        REP env "(def! load-file (fn* (f) (eval (read-string (str \"(do \" (slurp f) \")\")))))" |> ignore
+//        REP env "(defmacro! cond (fn* (& xs) (if (> (count xs) 0) (list 'if (first xs) (if (> (count xs) 1) (nth xs 1) (throw \"odd number of forms to cond\")) (cons 'cond (rest (rest xs)))))))" |> ignore
+//        REP env "(defmacro! or (fn* (& xs) (if (empty? xs) nil (if (= 1 (count xs)) (first xs) `(let* (or_FIXME ~(first xs)) (if or_FIXME or_FIXME (or ~@(rest xs))))))))" |> ignore
+
+        REP env """
+            (do
+                (def! *host-language* "fsharp by villep")
+                (def! not (fn* (a) (if a false true)))
+                (def! load-file (fn* (f) (eval (read-string (str "(do " (slurp f) ")")))))
+                (defmacro! cond (fn* (& xs) (if (> (count xs) 0) (list 'if (first xs) (if (> (count xs) 1) (nth xs 1) (throw "odd number of forms to cond")) (cons 'cond (rest (rest xs)))))))
+                (defmacro! or (fn* (& xs) (if (empty? xs) nil (if (= 1 (count xs)) (first xs) `(let* (or_FIXME ~(first xs)) (if or_FIXME or_FIXME (or ~@(rest xs))))))))
+                )
+            """ |> ignore
 
         if argv.Length >= 1 then
             let file = argv.[0]
